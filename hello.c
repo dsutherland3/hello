a1054917f144c3220f76f96e72b47941d21a8f1e
dsutherland3 <dsutherland3@huskers.unl.edu>
2025-08-28 12:32:34 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Daniel Sutherland
+ * Date: 2025/08/28
+ *
+ * A simple program to print my name in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Daniel Sutherland\n");
+
+  return 0;
+}
